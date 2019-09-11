5a1f50d9ae4a7dae67236149dfd79ac4c1e28975
Eduardo <eduardoapp.97@gmail.com>
2019-09-11 13:57:21 -0300

    fix cut functionality

--- src/js/Draw/L.PM.Draw.Cut.js
+++ src/js/Draw/L.PM.Draw.Cut.js
@@ -22,41 +22,64 @@ Draw.Cut = Draw.Polygon.extend({
       // exclude the drawn one
       .filter(l => l !== layer)
       // only layers with intersections
-      .filter(l => {
+      .filter((l) => {
         try {
-          return !!intersect(layer.toGeoJSON(15), l.toGeoJSON(15));
+          return !!intersect(layer.toGeoJSON(), l.toGeoJSON());
         } catch (e) {
-          /* eslint-disable-next-line no-console */
           console.error('You cant cut polygons with self-intersections');
           return false;
         }
       });
 
     // loop through all layers that intersect with the drawn (cutting) layer
-    layers.forEach(l => {
+    layers.forEach((l) => {
+      // the resulting layers after the cut
+      const resultingLayers = [];
+
       // find layer difference
-      const diff = difference(l.toGeoJSON(15), layer.toGeoJSON(15));
+      const diff = difference(l.toGeoJSON(), layer.toGeoJSON());
 
-      // the resulting layer after the cut
-      const resultingLayer = L.geoJSON(diff, l.options).addTo(this._map);
-      resultingLayer.addTo(this._map);
+      // if result is a multipolygon, split it into regular polygons
+      // TODO: remove as soon as multipolygons are supported
+      if (diff.geometry && diff.geometry.type === 'MultiPolygon') {
+        const geoJSONs = diff.geometry.coordinates.reduce((arr, coords) => {
+          arr.push({ type: 'Polygon', coordinates: coords });
+          return arr;
+        }, []);
 
-      // give the new layer the original options
-      resultingLayer.pm.enable(this.options);
-      resultingLayer.pm.disable();
+        // add new layers to map
+        geoJSONs.forEach((g) => {
+          const newL = L.geoJSON(g, l.options);
+          resultingLayers.push(newL);
+          newL.addTo(this._map);
+
+          // give the new layer the original options
+          newL.pm.enable(this.options);
+          newL.pm.disable();
+        });
+      } else {
+        // add new layer to map
+        const newL = L.geoJSON(diff, l.options).addTo(this._map);
+        resultingLayers.push(newL);
+        newL.addTo(this._map);
+
+        // give the new layer the original options
+        newL.pm.enable(this.options);
+        newL.pm.disable();
+      }
 
       // fire pm:cut on the cutted layer
       l.fire('pm:cut', {
         shape: this._shape,
-        layer: resultingLayer,
-        originalLayer: l,
+        layer: l,
+        resultingLayers,
       });
 
-      // fire pm:cut on the map
+      // fire pm:cut on the map for each cutted layer
       this._map.fire('pm:cut', {
         shape: this._shape,
-        layer: resultingLayer,
-        originalLayer: l,
+        cuttedLayer: l,
+        resultingLayers,
       });
 
       // add templayer prop so pm:remove isn't fired
@@ -66,22 +89,9 @@ Draw.Cut = Draw.Polygon.extend({
       // remove old layer and cutting layer
       l.remove();
       layer.remove();
-
-      if (resultingLayer.getLayers().length === 0) {
-        this._map.pm.removeLayer({ target: resultingLayer });
-      }
     });
   },
   _finishShape() {
-    // if self intersection is not allowed, do not finish the shape!
-    if (!this.options.allowSelfIntersection) {
-      this._handleSelfIntersection(false);
-
-      if (this._doesSelfIntersect) {
-        return;
-      }
-    }
-
     const coords = this._layer.getLatLngs();
     const polygonLayer = L.polygon(coords, this.options.pathOptions);
     this._cut(polygonLayer);
